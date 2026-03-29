9a9f9e47adfcd96e91912bb6517f28729fe689d7
tayabasherin-arch <tayabasherin@gmail.com>
2026-03-30 00:08:16 +0400

    Exercise 1: Coding is Cool
    Finished Exercise 1: Coding is Cool

---
+++ Exercise 1 - Coding is Cool.py
@@ -0,0 +1,11 @@
+# Assign the first word to the variable word1
+word1 = "Coding"
+
+# Assign the second word to the variable word2
+word2 = "is"
+
+# Assign the third word to the variable word3
+word3 = "Cool"
+
+# Print the sentence using string concatenation and variables
+print(word1 + word2 + word3)
