bcbba974e4d4a316ccc1d42823ce034596f361a4
tayabasherin-arch <tayabasherin@gmail.com>
2026-03-30 11:09:40 +0400

    Exercise 7: Some Counting
    Finished Exercise 7: Some Counting

---
+++ Exercise 7 - Some Counting.py
@@ -0,0 +1,19 @@
+# Loop that counts up from 0 to 50
+for i in range(51):
+    print(i)
+
+# Loop that counts down from 50 to 0
+for i in range(50, -1, -1):
+    print(i)
+
+# Loop that counts up from 30 to 50
+for i in range(30, 51):
+    print(i)
+
+# Loop that counts down from 50 to 10 in decrements of 2
+for i in range(50, 9, -2):
+    print(i)
+
+# Loop that counts up from 100 to 200 in increments of 5
+for i in range(100, 201, 5):
+    print(i)
